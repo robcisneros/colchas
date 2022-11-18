0b430f6dd8862906a06856e4e06b38d62663d698
robcisneros <robertocisneroslp@gmail.com>
2022-11-17 22:56:55 -0600

    images link fixed

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,6 +1,5 @@
 import { useRef, useState } from "react";
 import Input from "../../UI/Input";
-import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
   const [amountIsValid, setAmountIsValid] = useState(true);
